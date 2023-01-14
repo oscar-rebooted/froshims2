f6d736af759ea1bd53de6a9a1ca900b2369fb4d5
Oscar <oscar.of.oz@gmail.com>
2023-01-14 01:41:10 +0000

    Remove config.py from tracking

--- config.py
+++
@@ -1,7 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///froshims.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = 'mztEUcfSbLkW9HpnYgVu2J'
-SQLALCHEMY_ECHO = False
-
-#Should be False in production
-DEBUG = True
